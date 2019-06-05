b7655a1604345f653700c77b4ee1451f0c859abe
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2019-06-05 17:15:16 +0200

    Use POST request and parse JSON content to fill database.

--- mfxs.py
+++ mfxs.py
@@ -1,14 +1,26 @@
 from __future__ import print_function
-from flask import Flask
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+import datetime
+import time
+import json
 from influxdb import InfluxDBClient
 
 ### MACROS ###
+
+# HTTP server port.
+MFXS_HTTP_PORT = 8080
 
 # Sigfox frame lengths.
 SIGFOX_INTERMITTENT_WEATHER_DATA_FRAME_LENGTH_BYTES = 6
 SIGFOX_CONITNUOUS_WEATHER_DATA_FRAME_LENGTH_BYTES = 10
 SIGFOX_MONITORING_FRAME_LENGTH_BYTES = 9
 SIGFOX_GEOLOCATION_FRAME_LENGTH_BYTES = 11
+
+# Backend JSON headers.
+SIGFOX_BACKEND_JSON_HEADER_TIME = "time"
+SIGFOX_BACKEND_JSON_HEADER_DEVICE_ID = "device"
+SIGFOX_BACKEND_JSON_HEADER_DATA = "data"
 
 # Influx DB parameters.
 INFLUXDB_DATABASE_NAME = 'mfxdb'
@@ -31,53 +43,22 @@ INFLUXDB_SOLAR_CELL_VOLTAGE = "solar_cell_voltage"
 INFLUXDB_SUPERCAP_VOLTAGE = "supercap_voltage"
 INFLUXDB_MCU_VOLTAGE = "mcu_voltage"
 INFLUXDB_STATUS_BYTE = "status_byte"
-INFLUXDB_LATITUDE = "latitude"
-INFLUXDB_NORTH = "north"
-INFLUXDB_LONGITUDE = "longitude"
-INFLUXDB_EAST = "east"
+INFLUXDB_LOCATION = "location"
 INFLUXDB_ALTITUDE = "altitude"
 INFLUXDB_GPS_FIX_DURATION = "gps_fix_duration"
-INFLUXDB_COM_STATUS = "com_status"
 
-# Main program.
-print("\n*******************************************************")
-print("--------------- MeteoFox Server (MFXS) ----------------")
-print("*******************************************************\n")
+### FUNCTIONS DEFINITIONS ###
 
-# Create Influx DB client.
-influxdb_client = InfluxDBClient(host='localhost', port=INFLUXDB_HTTP_PORT)
+# Function to get current timestamp in pretty format.
+def MFXS_GetCurrentTimestamp():
+    return datetime.datetime.fromtimestamp(int(time.time())).strftime('%Y-%m-%d %H:%M:%S') + " *** "
 
-# Get list of existing databases.
-influxdb_database_list = influxdb_client.get_list_database()
-influxdb_mfxdb_found = False
-for influxdb_database in influxdb_database_list:
-    if (influxdb_database['name'].find(INFLUXDB_DATABASE_NAME) >= 0):
-        print("MeteoFox database found")
-        influxdb_mfxdb_found = True
-
-# Create MeteoFox database if it does not exist.   
-if (influxdb_mfxdb_found == False):
-    print("Creating database " + INFLUXDB_DATABASE_NAME)
-    influxdb_client.create_database(INFLUXDB_DATABASE_NAME)
-
-# Swtich to MeteoFox database.
-print("Switching to database " + INFLUXDB_DATABASE_NAME)
-influxdb_client.switch_database(INFLUXDB_DATABASE_NAME)
-
-# Start server.
-app = Flask(__name__)
+# Function for parsing Sigfox payload and fill database.
+def MFXS_FillDataBase(timestamp, device_id, data):
     
-@app.route("/")
-def index():
-    # Print message.
-    print("Root request received.")
-    return 'SPSWS server running.'
-    
-@app.route("/<device_id>/<data>")
-def action(device_id, data):
-    
-    # Format device ID.
-    device_id = device_id.upper()
+    # Format parameters.
+    influxdb_device_id = device_id.upper()
+    influxdb_timestamp = int(timestamp)
     
     # Monitoring frame.
     if len(data) == (2 * SIGFOX_MONITORING_FRAME_LENGTH_BYTES):
@@ -93,55 +74,56 @@ def action(device_id, data):
         json_body = [
         {
             "measurement": INFLUXDB_MCU_TEMPERATURE,
-            #"time": timestamp,
+            "time": influxdb_timestamp,
             "fields": {
-                device_id : mcu_temperature
+                influxdb_device_id : mcu_temperature
             }
         },
         {
             "measurement": INFLUXDB_PCB_TEMPERATURE,
-            #"time": timestamp,
+            "time": influxdb_timestamp,
             "fields": {
-                device_id : pcb_temperature
+                influxdb_device_id : pcb_temperature
             }
         },
         {
             "measurement": INFLUXDB_PCB_HUMIDITY,
-            #"time": timestamp,
+            "time": influxdb_timestamp,
             "fields": {
-                device_id : pcb_humidity
+                influxdb_device_id : pcb_humidity
             }
         },
         {
             "measurement": INFLUXDB_SOLAR_CELL_VOLTAGE,
-            #"time": timestamp,
+            "time": influxdb_timestamp,
             "fields": {
-                device_id : solar_cell_voltage
+                influxdb_device_id : solar_cell_voltage
             }
         },
         {
             "measurement": INFLUXDB_SUPERCAP_VOLTAGE,
-            #"time": timestamp,
+            "time": influxdb_timestamp,
             "fields": {
-                device_id : supercap_voltage
+                influxdb_device_id : supercap_voltage
             }
         },
         {
             "measurement": INFLUXDB_MCU_VOLTAGE,
-            #"time": timestamp,
+            "time": influxdb_timestamp,
             "fields": {
-                device_id : mcu_voltage
+                influxdb_device_id : mcu_voltage
             }
         },
         {
             "measurement": INFLUXDB_STATUS_BYTE,
-            #"time": timestamp,
+            "time": influxdb_timestamp,
             "fields": {
-                device_id : status_byte
+                influxdb_device_id : status_byte
             }
         }]
+        print(json_body)
         # Fill data base.
-        influxdb_client.write_points(json_body)
+        influxdb_client.write_points(json_body, time_precision='s')
         print("Monitoring data stored in database.")
     
     # Weather data frame.
@@ -156,41 +138,42 @@ def action(device_id, data):
         json_body = [
         {
             "measurement": INFLUXDB_TEMPERATURE,
-            #"time": timestamp,
+            "time": influxdb_timestamp,
             "fields": {
-                device_id : temperature
+                influxdb_device_id : temperature
             }
         },
         {
             "measurement": INFLUXDB_HUMIDITY,
-            #"time": timestamp,
+            "time": influxdb_timestamp,
             "fields": {
-                device_id : humidity
+                influxdb_device_id : humidity
             }
         },
         {
             "measurement": INFLUXDB_LIGHT,
-            #"time": timestamp,
+            "time": influxdb_timestamp,
             "fields": {
-                device_id : light
+                influxdb_device_id : light
             }
         },
         {
             "measurement": INFLUXDB_UV_INDEX,
-            #"time": timestamp,
+            "time": influxdb_timestamp,
             "fields": {
-                device_id : uv_index
+                influxdb_device_id : uv_index
             }
         },
         {
             "measurement": INFLUXDB_PRESSURE,
-            #"time": timestamp,
+            "time": influxdb_timestamp,
             "fields": {
-                device_id : pressure
+                influxdb_device_id : pressure
             }
         }]
+        print(json_body)
         # Fill data base.
-        influxdb_client.write_points(json_body)
+        influxdb_client.write_points(json_body, time_precision='s')
         print("Weather data stored in database.")
         
         # Weather data frame.
@@ -220,31 +203,89 @@ def action(device_id, data):
             # Create JSON object.
             json_body = [
             {
-                "measurement": INFLUXDB_ALTITUDE,
-                #"time": timestamp,
+                "measurement": INFLUXDB_LOCATION,
+                "time": influxdb_timestamp,
                 "fields": {
-                    device_id : altitude
+                    influxdb_device_id : latitude,
+                    influxdb_device_id : longitude
+                }
+            },
+            {
+                "measurement": INFLUXDB_ALTITUDE,
+                "time": influxdb_timestamp,
+                "fields": {
+                    influxdb_device_id : altitude
                 }
             },
             {
                 "measurement": INFLUXDB_GPS_FIX_DURATION,
-                #"time": timestamp,
-                "tags": {
-                    "key" : "FR",
-                    "latitude" : latitude,
-                    "longitude" : longitude,
-                    "name" : device_id
-                },
+                "time": influxdb_timestamp,
                 "fields": {
-                    device_id : gps_fix_duration
+                    influxdb_device_id : gps_fix_duration
                 }
             }]
+            print(json_body)
             # Fill data base.
-            influxdb_client.write_points(json_body)
+            influxdb_client.write_points(json_body, time_precision='s')
             print("Geolocation data stored in database.")
-    
-    # Print message.
-    return 'Data successfully stored'
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+### CLASS DECLARATIONS ###
+
+class ServerHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        print(MFXS_GetCurrentTimestamp() + "GET request received")
+        self.send_response(200)
+        
+    def do_HEAD(self):
+        print(MFXS_GetCurrentTimestamp() + "HEAD request received")
+        self.send_response(200)
+        
+    def do_POST(self):
+        # Get JSON content.
+        post_length = int(self.headers.getheader('content-length', 0))
+        post_json = json.loads(self.rfile.read(post_length))
+        print(MFXS_GetCurrentTimestamp() + "POST request received: " + str(post_json))
+        # Parse data.
+        callback_timestamp = post_json[SIGFOX_BACKEND_JSON_HEADER_TIME]
+        #timestamp = timestamp + "000000000" # Convert Unix timestamp to nanoseconds for InfluxDB.
+        callback_device_id = post_json[SIGFOX_BACKEND_JSON_HEADER_DEVICE_ID]
+        callback_data = post_json[SIGFOX_BACKEND_JSON_HEADER_DATA]
+        print("Uplink message: timestamp=" + callback_timestamp + " device_id=" + callback_device_id + " data=" + callback_data)
+        # Fill database.
+        MFXS_FillDataBase(int(callback_timestamp), callback_device_id, callback_data)
+        # Send HTTP response.
+        self.send_response(200)
+
+### MAIN PROGRAM ###
+
+print("\n*******************************************************")
+print("--------------- MeteoFox Server (MFXS) ----------------")
+print("*******************************************************\n")
+
+# Create Influx DB client.
+influxdb_client = InfluxDBClient(host='localhost', port=INFLUXDB_HTTP_PORT)
+
+# Get list of existing databases.
+influxdb_database_list = influxdb_client.get_list_database()
+influxdb_mfxdb_found = False
+for influxdb_database in influxdb_database_list:
+    if (influxdb_database['name'].find(INFLUXDB_DATABASE_NAME) >= 0):
+        print(MFXS_GetCurrentTimestamp() + "MeteoFox database found")
+        influxdb_mfxdb_found = True
+
+# Create MeteoFox database if it does not exist.   
+if (influxdb_mfxdb_found == False):
+    print(MFXS_GetCurrentTimestamp() + "Creating database " + INFLUXDB_DATABASE_NAME)
+    influxdb_client.create_database(INFLUXDB_DATABASE_NAME)
+
+# Swtich to MeteoFox database.
+print(MFXS_GetCurrentTimestamp() + "Switching to database " + INFLUXDB_DATABASE_NAME)
+influxdb_client.switch_database(INFLUXDB_DATABASE_NAME)
+        
+# Start server.
+SocketServer.TCPServer.allow_reuse_address = True
+mfxs_handler = ServerHandler
+mfxs = SocketServer.TCPServer(("", MFXS_HTTP_PORT), mfxs_handler)
+print (MFXS_GetCurrentTimestamp() + "Starting server at port", MFXS_HTTP_PORT)
+mfxs.serve_forever()
